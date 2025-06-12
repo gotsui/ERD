6067b07c61e1011e91cad1f14975807fe9a7cbfe
gotsui <krhamui.2016@gmail.com>
2025-06-12 20:55:58 +0900

    配置変更

--- project/src/app/page.tsx
+++ project/src/app/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const Home: React.FC = () => {
     return (
-        <div className="container px-4">
+        <div className="container mx-auto p-4">
             <div className="grid grid-cols-3 gap-4">
                 <Link
                     href="/erd"
